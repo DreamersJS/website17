daa8a36ee719b239d4286e00030bf3a630548a33
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2025-03-17 15:32:21 +0200

    add adminindex component for more security verify user role in db

--- website-1712/client/src/components/AdminIndex.jsx
+++ website-1712/client/src/components/AdminIndex.jsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Admin from './Admin';
+import { useRecoilValue } from "recoil";
+import { userState } from "../recoil/userAtom";
 
 const AdminIndex = () => {
   const [loading, setLoading] = useState(true);
   const [isAdmin, setIsAdmin] = useState(false);
   const navigate = useNavigate();
-
+  const user = useRecoilValue(userState);
+  
   useEffect(() => {
     const checkAdmin = async () => {
       try {
-        const response = await fetch('/api/coaches/admin', {
+        const response = await fetch(`/api/coaches/admin/${user.id}`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('authToken')}`,
           },
         });
-
-        if (response.data.role === 'ADMIN') {
+        if (!response.ok) {
+          throw new Error('Failed to verify admin role');
+        }
+        const data = await response.json();
+        if (data.role === 'ADMIN') {
           setIsAdmin(true);
         } else {
           navigate('/'); // Redirect if not admin
@@ -34,10 +41,7 @@ const AdminIndex = () => {
   if (loading) return <div>Loading...</div>;
 
   return(
-    isAdmin ? <div>Welcome to Admin Panel
-        <div>Admin Content</div>
-        
-    </div> : null
+    isAdmin ? <Admin/> : null
   ) 
 };
 export default AdminIndex; 
