99e51f1713be5320b6d49599b53d11d5955ef1a0
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2026-06-14 21:20:20 +0300

    chore(tests) auth tests update

--- server/__tests__/integration/auth.test.js
+++ server/__tests__/integration/auth.test.js
@@ -3,116 +3,171 @@ import { describe, it, expect, jest } from "@jest/globals";
 import app from "../../src/app.js";
 
 describe("Auth API", () => {
-  describe("POST /api/users/register", () => {
-    it("should register a new user", async () => {
-      const res = await request(app)
-        .post("/api/users/register")
-        .send({
-          username: "TestUser",
-          email: `test${Date.now()}@mail.com`,
-          password: "123456",
-        });
+  it("should register a new user", async () => {
+    const res = await request(app)
+      .post("/api/users/register")
+      .send({
+        username: "TestUser",
+        email: `test${Date.now()}@mail.com`,
+        password: "123456",
+      });
 
-      expect(res.statusCode).toBe(201);
-      expect(res.body.data).toHaveProperty("id");
-      expect(res.body.data.email).toBeDefined();
-      expect(res.body.message).toBe("User created successfully");
+    expect(res.statusCode).toBe(201);
+    expect(res.body.data).toHaveProperty("id");
+    expect(res.body.data.id.trim().length).toBeGreaterThan(0);
+
+    expect(res.body.data.email).toBeDefined();
+    expect(res.body.data.email).toEqual(expect.any(String));
+    expect(res.body.data.email).toMatch(/@/);
+
+    expect(res.body.data.username).toEqual(expect.any(String));
+    expect(res.body.data.username.trim()).not.toBe('');
+
+    expect(res.body.data.password).not.toBeDefined();
+
+    expect(res.body.message).toMatch(/successfully/i);
+  });
+
+  it("should fail if email already exists", async () => {
+    const email = `test${Date.now()}@mail.com`;
+
+    await request(app).post("/api/users/register").send({
+      username: "User1",
+      email,
+      password: "123456",
     });
 
-    it("should fail if email already exists", async () => {
-      const email = `test${Date.now()}@mail.com`;
+    const res = await request(app).post("/api/users/register").send({
+      username: "User2",
+      email,
+      password: "123456",
+    });
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+    // expect(res.statusCode).toBe(400);// returns 500 - I throw an error, but in the controller I don’t handle it, just pass it to Express. So Express default error handler kicks in & Returns 500 Internal Server Error = test gets 500 instead of 400
+    expect(res.body.error).toBe("Email is already in use.");
+  });
 
-      await request(app).post("/api/users/register").send({
-        username: "User1",
-        email,
-        password: "123456",
-      });
+  it("should fail if missing username",async () => {
+    const email = `test${Date.now()}@mail.com`;
 
-      const res = await request(app).post("/api/users/register").send({
-        username: "User2",
-        email,
-        password: "123456",
-      });
-      expect(res.statusCode).toBeGreaterThanOrEqual(400);
-      // expect(res.statusCode).toBe(400);// returns 500 - I throw an error, but in the controller I don’t handle it, just pass it to Express. So Express default error handler kicks in & Returns 500 Internal Server Error = test gets 500 instead of 400
-      expect(res.body.error).toBe("Email is already in use.");
+    const res = await request(app).post("/api/users/register").send({
+      email,
+      password: "123456",
+    });
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+  })
+  it("should fail if empty username",async () => {
+    const email = `test${Date.now()}@mail.com`;
+
+    const res = await request(app).post("/api/users/register").send({
+      username:'',
+      email,
+      password: "123456",
+    });
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+  })
+  it("should fail if whitespace username",async () => {
+    const email = `test${Date.now()}@mail.com`;
+
+    const res = await request(app).post("/api/users/register").send({
+      username:'   ',
+      email,
+      password: "123456",
+    });
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+  })
+  it("should fail if too short username",async () => {
+    const email = `test${Date.now()}@mail.com`;
+
+    const res = await request(app).post("/api/users/register").send({
+      username:'S',
+      email,
+      password: "123456",
+    });
+    expect(res.statusCode).toBeGreaterThanOrEqual(400);
+  })
+});
+
+
+describe("Login", () => {
+  let email = `test${Date.now()}@mail.com`;
+  let password = "123456";
+
+  beforeEach(async () => {
+    await request(app).post("/api/users/register").send({
+      username: "LoginUser",
+      email,
+      password,
     });
   });
 
-  describe("POST /api/users/login", () => {
-    let email = `test${Date.now()}@mail.com`;
-    let password = "123456";
-
-    beforeEach(async () => {
-      await request(app).post("/api/users/register").send({
-        username: "LoginUser",
-        email,
-        password,
-      });
+  it("should login successfully", async () => {
+    const res = await request(app).post("/api/users/login").send({
+      email,
+      password,
     });
 
-    it("should login successfully", async () => {
-      const res = await request(app).post("/api/users/login").send({
-        email,
-        password,
-      });
+    expect(res.statusCode).toBe(200);
+    // expect(res.headers["set-cookie"]).toBeDefined();
+    expect(res.body.message).toMatch(/successful/i);
+    expect(res.body.data).toHaveProperty("id");
+    expect(res.body.data.id.trim().length).toBeGreaterThan(0);
 
-      expect(res.statusCode).toBe(200);
-      expect(res.headers["set-cookie"]).toBeDefined();
-      expect(res.body.message).toBe("Login successful");
+    expect(res.body.data.email).toBeDefined();
+    expect(res.body.data.email).toEqual(expect.any(String));
+    expect(res.body.data.email).toMatch(/@/);
+
+    expect(res.body.data.username).toEqual(expect.any(String));
+    expect(res.body.data.username.trim()).not.toBe('');
+
+    expect(res.body.data.password).not.toBeDefined();
+  })
+  it("should fail with wrong password", async () => {
+    const res = await request(app).post("/api/users/login").send({
+      email,
+      password: "wrongpassword",
     });
 
-    it("should fail with wrong password", async () => {
-      const res = await request(app).post("/api/users/login").send({
-        email,
-        password: "wrongpassword",
-      });
-
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("Invalid credentials.");
-    });
-
-    it("should fail if user does not exist", async () => {
-      const res = await request(app).post("/api/users/login").send({
-        email: "nonexistent@mail.com",
-        password: "123456",
-      });
-
-      expect(res.statusCode).toBe(401);
-      expect(res.body.error).toBe("User not found.");
-    });
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("Invalid credentials.");
   });
 
-  describe("Auth-protected route", () => {
-    // works if I remove verifyAdmin from route
-    let agent;
-    //those test probably aint valid anymore cos of the change cookie-> auth headers
-    beforeEach(async () => {
-      const email = `auth${Date.now()}@mail.com`;
-      agent = request.agent(app);
-
-      await request(app).post("/api/users/register").send({
-        username: "AuthUser",
-        email,
-        password: "123456",
-      });
-
-      const loginRes = await agent.post("/api/users/login").send({
-        email,
-        password: "123456",
-      });
+  it("should fail if user does not exist", async () => {
+    const res = await request(app).post("/api/users/login").send({
+      email: "nonexistent@mail.com",
+      password: "123456",
     });
 
-    it("should allow access with valid cookie", async () => {
-      const res = await agent.get("/api/users/all");
+    expect(res.statusCode).toBe(401);
+    expect(res.body.error).toBe("User not found.");
+  });
+});
 
-      expect(res.statusCode).toBe(200);
+describe("Auth-protected route", () => {
+  let token;
+  beforeEach(async () => {
+    const email = `auth${Date.now()}@mail.com`;
+
+    const registerRes = await request(app).post("/api/users/register").send({
+      username: "AuthUser",
+      email,
+      password: "123456",
     });
 
-    it("should block access without cookie", async () => {
-      const res = await request(app).get("/api/users/all");
+    token = registerRes.body.meta.accessToken;
+  });
 
-      expect(res.statusCode).toBe(401);
-    });
+  it("should allow access with valid header token", async () => {
+    const res = await request(app).get("/api/users/logout").set('Authorization',`Bearer ${token}`);
+
+    // maybe should make it 204?
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toMatch(/successfully/i)
+  });
+
+  it("should block access without header token", async () => {
+    const res = await request(app).get("/api/users/logout");
+
+    expect(res.statusCode).toBe(401);
   });
 });
